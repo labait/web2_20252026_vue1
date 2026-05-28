6969806931195fba959f54be7d1f1906973ade74
Antonio Molinari <antoniomolinari@me.com>
2026-05-28 16:39:29 +0200

    feat: add deploy script to package.json and update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web2_20252026_vue1/',
   plugins: [
     vue(),
     tailwindcss()
